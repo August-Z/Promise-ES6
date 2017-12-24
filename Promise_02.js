8d5181233ab304a84fe1594d50b10696442ea704
August-Z <402279076@qq.com>
2017-12-24 13:56:10 +0800

    Promise 对象使用注意事项

---
+++ Promise_02.js
@@ -0,0 +1,22 @@
+/**
+ * 在 Promise_01.js 中我们创建了一个 Promise 对象，但是我们只是 new 了一个对象，
+ * 我们传进去的函数就已经执行，这是一个需要注意的细节。所以用 Promise 一般都是包在一个函数中
+ * 然后运行这个函数。
+ */
+(function () {
+
+    function runAsync() {
+
+        const p = new Promise((resolve, reject) => {
+
+            // 做一些异步操作
+            setTimeout(() => {
+                console.log('执行完成');
+                resolve('Query OK ！');
+            })
+
+        })
+
+    }
+
+})();
